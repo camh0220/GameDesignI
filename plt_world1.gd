853c689118deb92c024b6fe397fafa5e6925bac5
camh0220 <helling.c1@stu.janesville.k12.wi.us>
2024-02-20 13:42:57 -0600

    update

--- plt_world1.gd
+++ plt_world1.gd
@@ -13,3 +13,17 @@ func _on_rigid_body_2d_mouse_entered():
 		
 		
 
+var dragging = false
+var offset = Vector2.ZERO
+
+func _process(delta):
+	if dragging:
+		$draggable.global_position = get_global_mouse_position() - offset
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+
+func _on_button_button_up():
+	dragging = false
