447cfa646c5fd0d256883d9abebcb434609fc2e5
camh0220 <helling.c1@stu.janesville.k12.wi.us>
2024-03-06 13:48:20 -0600

    update

--- phys_demo.gd
+++ phys_demo.gd
@@ -7,6 +7,12 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var nball = preload("res://phys_ball.tscn").instantiate()
 		nball.position = event.position
+		var v_intertia = float($Panel/txtInertia.text)
+		var v_x = float($Panel/txtVelX.text)
+		var v_y = float($Panel/txtVelY.text)
+		var velocity = Vector2(v_x, v_y)
+		nball.inertia = v_intertia
+		nball.linear_velocity = velocity
 		add_child(nball)
 
 func _on_grav_slider_value_changed(value):
@@ -20,3 +26,14 @@ func _on_button_pressed():
 		if child is RigidBody2D:
 			var force = randi_range(50, 1000)
 			child.apply_central_impulse(Vector2(1,0) * force)
+
+
+func _on_button_2_pressed():
+	var v_intertia = float($Panel/txtInertia.text)
+	var v_x = float($Panel/txtVelX.text)
+	var v_y = float($Panel/txtVelY.text)
+	var velocity = Vector2(v_x, v_y)
+	for child in get_children():
+		if child is RigidBody2D:
+			child.inertia = v_intertia
+			child.linear_velocity = velocity
