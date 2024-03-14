970f1f247b04db41617c4e304d1798fbd03bd066
camh0220 <helling.c1@stu.janesville.k12.wi.us>
2024-03-14 13:42:22 -0500

    update 03/14/24

--- td_player.gd
+++ td_player.gd
@@ -17,6 +17,7 @@ func _physics_process(delta):
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
 	)
+	update_animation(direction)
 	if direction.length() > 0:
 		look_direction = direction
 		# Scale to 1 to prevent speed boost from diagonals
@@ -26,8 +27,24 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2(), SPEED)
 	velocity += inertia
 	move_and_slide()
-	inertia = inertia.move_teamed(Vecto2(), delta * 100.0)
+	inertia = inertia.move_teamed(Vector2(), delta * 100.0)
 	
-	if Input is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
+
+func update_animation(direction):
+	var a_name = "idle_"
+	if direction.length() > 0:
+		look_direction = direction
+		a_name = "walk_"
+	if look_direction.x != 0:
+		a_name += "side"
+		$AnimatedSprite2D.flip_h = look_direction.x < 0
+	elif look_direction.y < 0:
+		a_name += "up"
+	elif look_direction.y > 0:
+		a_name += "down"
+	
+	$AnimatedSprite2D.animation = a_name
+	$AnimatedSprite2D.play()
