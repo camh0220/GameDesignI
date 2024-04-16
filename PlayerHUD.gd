0d02613601a8d82c48c725d9c5de981f2af069f3
camh0220 <helling.c1@stu.janesville.k12.wi.us>
2024-04-16 13:42:32 -0500

    Update 04/16/24

--- PlayerHUD.gd
+++ PlayerHUD.gd
@@ -19,22 +19,24 @@ func draw_hearts():
 	for heart in hearts.get_children():
 		hearts.remove_child(heart)
 	for i in range(int(player.data.max_health) / 20):
-		create_heart() # 1 heart per 20 hp
+		create_heart()  # 1 heart per 20 hp
 
 func _ready():
 	draw_hearts()
-	
+
 func _process(delta):
+	$Money/Coins.text = "%03d" % player.data.money
 	var p_health = player.data.health
 	var full_hearts = floor(p_health / 20)
 	var remainder = int(p_health) % 20
+	
 	for heart in hearts.get_children():
 		var index = heart.get_index()
 		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
 		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
 		heart.position = Vector2(x, y)
 		
-		
+		# Frame 8 empty, 7 1/4, 6 half, 5 3/4, 4 full
 		if index > full_hearts:
 			heart.frame = 8
 		elif index == full_hearts:
