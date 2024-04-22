91a344ac8bbe028c3085fdd0326dd9b888c97691
camh0220 <143837486+camh0220@users.noreply.github.com>
2024-04-22 13:43:26 -0500

    Update 04/22/24

--- entities/objects/Node2D.gd
+++ entities/objects/Node2D.gd
@@ -1,11 +1,43 @@
 extends Node2D
 
+# Use -1 for default value; multiple of the same item should be enumerated.
+# Edit contents in inspector.
+@export var contents: Dictionary = {} # { "coin1": 5, "coin2": 10, "miniheart": -1 }
+var OPEN_STATE = false
+var regex = RegEx.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func in_range(player) -> bool:
+	return player.data.state != player.STATES.DEAD and \
+		   $StaticBody2D/Area2D.overlaps_body(player)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func interact(player):
+	if not OPEN_STATE:
+		open_chest(player)
+
+
+func open_chest(player):
+	OPEN_STATE = true
+	$Sprite2D.frame = 1
+	for item in contents.keys():
+		drop_item(item, contents[item], player)
+		await get_tree().create_timer(0.5).timeout
+	contents = {}
+
+
+func bounce_towards_player(item, player):
+	var direction = (player.global_position - global_position).normalized()
+	var BOUNCE_DISTANCE = 50.0
+	var bounce_path = direction * BOUNCE_DISTANCE + \
+					  Vector2(randf_range(10.0, 10.0), randf_range(-10.0, 10.0))
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(item, "global_position", item.global_position + bounce_path, 0.5)
+	tween.play()
+	await tween.finished
+	
+	
+	func drop_item(item_name, value, player):
+		pass
