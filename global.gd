2d0d6191d2fb659c73fd71c3b834ef39d7f0b4e0
camh0220 <143837486+camh0220@users.noreply.github.com>
2024-05-22 13:53:45 -0500

    Update 05/22/24

--- global.gd
+++ global.gd
@@ -2,8 +2,9 @@ extends Node
 
 var player_lives = 3
 
+
 func spawn_mushroom(pos):
-	var MushroomScene = load("res://1upMushroom.png")
+	var MushroomScene = load("res://mushroom.tscn")
 	var mushroom = MushroomScene.instantiate()
 	mushroom.global_position = pos
 	get_tree().root.add_child(mushroom)
--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var is_dying = false
 var is_jumping = false
-
+var is_big = false
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
@@ -53,7 +53,10 @@ func _on_hitbox_body_entered(body):
 		return
 		
 	if body.is_in_group("Enemy") and body.is_alive:
-		die()
+		if is_big:
+			become_small()
+		else:
+			die()
 		
 func die():
 	if is_dying:
@@ -84,3 +87,11 @@ func move_player_up_and_down():
 		
 func on_DeathTimer_timeout():
 	get_tree().reload_current_scene()
+	
+func become_big():
+	is_big = true
+	self.scale = Vector2(1.5, 1.5)
+
+func become_small():
+	is_big = false
+	self.scale = Vector2(1, 1)
