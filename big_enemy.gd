3d23c701271b5aa33edb9780c4f9ddc09fb4ecaf
camh0220 <143837486+camh0220@users.noreply.github.com>
2024-05-31 13:42:53 -0500

    Update 05/31/24

--- big_enemy.gd
+++ big_enemy.gd
@@ -1,28 +1,45 @@
 extends CharacterBody2D
-# Video @ 5:23
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var SPEED = -25.0
+var facing_right = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_alive = true
+var timer = 1.0
+var timerStarted : bool
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var sprite_2d = $Sprite2D
+@export var speed: float
+@export var deathSprite : Texture2D
 
+func _ready():
+	add_to_group("Enemy")
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	velocity.x = SPEED
+	
 	move_and_slide()
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Player"):
+		is_alive = false
+		die()
+
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		SPEED = abs(SPEED)
+	else:
+		SPEED = abs(SPEED) * -1
+
+func die():
+	collision_shape_2d.queue_free()
+	sprite_2d.texture = deathSprite
+	velocity.x = 0
+	timerStarted = true
